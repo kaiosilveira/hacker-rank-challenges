cf36a183a6643630f750866d0094d547e4fbcacb
Kaio Silveira <silveira.kaio@icloud.com>
2022-08-26 21:00:50 +0100

    sub-array-division: update benchmark

--- sub-array-division/benchmarking.rb
+++ sub-array-division/benchmarking.rb
@@ -1,14 +1,15 @@
 require_relative '../_utils/benchmark.rb'
 require_relative '../_utils/array/array.rb'
-require_relative './select-chocolate-slices-matching-criteria.rb'
+require_relative './select_chocolate_slices_matching_criteria.rb'
+
+ENV["ALLOW_UNCONSTRAINED_ARRAYS_ENABLED"] = "enabled"
+MAX_N_SIZE = 100_000
 
 include ArrayUtils
 include BenchmarkUtils
 
-MAX_N_SIZE = 100_000
-
 def execute_sub_array_division_with_array_of_size(n)
-  select_chocolate_slices_matching_criteria(
+  HackerRank::Algorithms.select_chocolate_slices_matching_criteria(
     birth_day: 4,
     birth_month: 2,
     chocolate: ArrayUtils.create_array_of_random_integers(size: n)
