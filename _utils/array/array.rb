13018c6e163ade9d5f51ee5bfcbc70f16713da67
Kaio Silveira <silveira.kaio@icloud.com>
2022-08-27 17:17:03 +0100

    allow client code to specify the max of each integer when creating arrays

--- _utils/array/array.rb
+++ _utils/array/array.rb
@@ -3,7 +3,7 @@ require_relative '../number/number.rb'
 module ArrayUtils
   include NumberUtils
 
-  def create_array_of_random_integers(size:)
-    (0..size - 1).to_a.map { [1, create_random_integer].max }
+  def create_array_of_random_integers(size:, item_max: 100)
+    (0..size - 1).to_a.map { [1, create_random_integer(max: item_max)].max }
   end
 end
