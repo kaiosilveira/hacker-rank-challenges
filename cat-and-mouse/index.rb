1b497b77b69b52357a25f3f43d34a60f335c889b
Kaio Silveira <silveira.kaio@icloud.com>
2022-08-28 11:20:17 +0100

    cat-and-mouse: run challenge against samples
    Co-Authored-By: Daniel Silveira <danieldoamaral10@gmail.com>
    Co-Authored-By: Raul Silva <adraulpesilva@gmail.com>

--- cat-and-mouse/index.rb
+++ cat-and-mouse/index.rb
@@ -1,40 +1,13 @@
-require 'test/unit'
+require_relative './challenge.rb'
+require_relative '../_utils/hacker-rank/hacker-rank.rb'
 
-def cat_and_mouse(catA, catB, mouse)
-  cat_a_distance = (mouse - catA).abs
-  cat_b_distance = (mouse - catB).abs
+samples = [
+  HackerRank.create_sample(expected_result: 'Cat B', params: [2, 5, 4]),
+  HackerRank.create_sample(expected_result: 'Cat B', params: [1, 2, 3]),
+  HackerRank.create_sample(expected_result: 'Mouse C', params: [1, 3, 2]),
+]
 
-  return 'Cat A' if cat_a_distance < cat_b_distance
-  return 'Cat B' if cat_b_distance < cat_a_distance
-
-  'Mouse C'
-end
-
-class CatAndMouseTest < Test::Unit::TestCase
-  def test_cat_A_gets_the_mouse
-    catA = 3
-    catB = 4
-    mouse = 2
-    expected_result = 'Cat A'
-
-    assert_equal expected_result, cat_and_mouse(catA, catB, mouse)
-  end
-
-  def test_cat_B_gets_the_mouse
-    catA = 2
-    catB = 5
-    mouse = 4
-    expected_result = 'Cat B'
-
-    assert_equal expected_result, cat_and_mouse(catA, catB, mouse)
-  end
-
-  def test_mouse_escapes
-    catA = 2
-    catB = 2
-    mouse = 4
-    expected_result = 'Mouse C'
-
-    assert_equal expected_result, cat_and_mouse(catA, catB, mouse)
-  end
+HackerRank::Runner.new(samples).run do |*params|
+  catA, catB, mouse = params
+  HackerRank::Challenges.cat_and_mouse(catA, catB, mouse)
 end
