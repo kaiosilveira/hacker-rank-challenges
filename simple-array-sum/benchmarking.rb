8af76f756bab71a6aa1bbc25869a8997df9cd179
Kaio Silveira <silveira.kaio@icloud.com>
2022-08-28 13:33:17 +0100

    simple-array-sum: update usage of the Benchmarking module

--- simple-array-sum/benchmarking.rb
+++ simple-array-sum/benchmarking.rb
@@ -3,7 +3,6 @@ require_relative '../_utils/benchmark.rb'
 require_relative '../_utils/array/array.rb'
 
 include ArrayUtils
-include BenchmarkUtils
 
 MAX_N_SIZE = 10000
 
@@ -13,7 +12,7 @@ def execute_simple_array_sum_with_array_of_size(n)
   )
 end
 
-BenchmarkUtils.create_time_complexity_analysis(
+HackerRank::Benchmarking.create_time_complexity_analysis(
   method_name: :execute_simple_array_sum_with_array_of_size,
   n_max: MAX_N_SIZE,
   n_incrementation_step: 1000,
