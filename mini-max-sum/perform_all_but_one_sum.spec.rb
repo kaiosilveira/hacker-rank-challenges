70f52866b7101610a692fca550b06db664c87a69
Kaio Silveira <silveira.kaio@icloud.com>
2022-08-30 08:44:38 +0100

    mini-max-sum: fix test suite

--- mini-max-sum/perform_all_but_one_sum.spec.rb
+++ mini-max-sum/perform_all_but_one_sum.spec.rb
@@ -1,11 +1,13 @@
 require 'test/unit'
 require_relative './perform_all_but_one_sum.rb'
 require_relative '../_utils/array/array.rb'
+require_relative '../_utils/number/number.rb'
 
 module HackerRank
   module Challenges
     class PerformAllButOneSumTest < Test::Unit::TestCase
       include ArrayUtils
+      include NumberUtils
 
       def teardown
         ENV["ALLOW_UNCONSTRAINED_ARRAYS_ENABLED"] = 'disabled'
@@ -13,7 +15,7 @@ module HackerRank
 
       def test_raises_exception_if_array_has_more_than_5_items_and_flag_is_disabled
         ENV["ALLOW_UNCONSTRAINED_ARRAYS_ENABLED"] = 'disabled'
-
+        numbers = create_array_of_random_integers(size: 6)
         exception = Exceptions::InputOutOfConstraintsException.new(
           input_name: 'numbers',
           constraints: '1 <= numbers.size <= 5'
@@ -21,17 +23,18 @@ module HackerRank
 
         assert_raises exception do
           Algorithms.perform_all_but_one_sum(
-            numbers: create_array_of_random_integers(size: 5),
-            number_to_exclude: 1
+            numbers: numbers,
+            number_to_exclude: numbers[create_random_integer(max: numbers.size)]
           )
         end
       end
 
       def test_executes_if_array_has_more_than_5_items_and_flag_is_enabled
         ENV["ALLOW_UNCONSTRAINED_ARRAYS_ENABLED"] = 'enabled'
+        numbers = create_array_of_random_integers(size: 5)
         assert Algorithms.perform_all_but_one_sum(
-          numbers: create_array_of_random_integers(size: 5),
-          number_to_exclude: 1
+          numbers: numbers,
+          number_to_exclude: numbers[create_random_integer(max: numbers.size)]
         )
       end
 
