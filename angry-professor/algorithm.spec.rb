52f424e0465680575ed350fd680a499de3967f92
Kaio Silveira <silveira.kaio@icloud.com>
2022-10-14 00:08:57 +0100

    angry-professor: add test suite

---
+++ angry-professor/algorithm.spec.rb
@@ -0,0 +1,17 @@
+require "test/unit"
+require_relative "./algorithm.rb"
+require_relative "../_utils/exceptions/exceptions.rb"
+
+module HackerRank
+  module Algorithms
+    class AlgorithmTests < Test::Unit::TestCase
+      include HackerRank::Exceptions
+
+      def test_applies_the_pattern_correctly
+        assert_false Algorithms.was_class_canceled?(arrival_times: [-2, -1, 0, 1, 2], threshold: 3)
+        assert Algorithms.was_class_canceled?(arrival_times: [-1, -3, 4, 2], threshold: 3)
+        assert_false Algorithms.was_class_canceled?(arrival_times: [0, -1, 2, 1], threshold: 2)
+      end
+    end
+  end
+end
