11dba44d406ecc3df78747d3de8484981206eca1
Kaio Silveira <silveira.kaio@icloud.com>
2022-10-15 17:04:31 +0100

    sub-array-division: update code to use new validators and exceptions

--- sub-array-division/select_chocolate_slices_matching_criteria.rb
+++ sub-array-division/select_chocolate_slices_matching_criteria.rb
@@ -1,14 +1,15 @@
-require_relative '../_utils/exceptions/exceptions.rb'
+require_relative "../_utils/exceptions/exceptions.rb"
+require_relative "../_utils/validation/index.rb"
 
 module HackerRank
   module Algorithms
     include HackerRank::Exceptions
+    include HackerRank::Validation
 
     def self.select_chocolate_slices_matching_criteria(birth_day:, birth_month:, chocolate:)
-      allow_unconstrained_arrays = ENV["ALLOW_UNCONSTRAINED_ARRAYS_ENABLED"] == 'enabled'
-      raise InvalidDayOfMonthException if birth_day < 1 || birth_day > 31
-      raise InvalidMonthOfYearException if birth_month < 1 || birth_month > 12
-      raise PossiblyUnboundArrayException if chocolate.size > 100 && !allow_unconstrained_arrays
+      Validation.validate_day_of_month(input_name: "birth_day", value: birth_day)
+      Validation.validate_month_of_year(input_name: "birth_month",value: birth_month)
+      Validation.ensure_max_array_size(input_name: 'chocolate', value: chocolate, max_size: 100)
 
       chocolate.filter.with_index do |_, index|
         slice = chocolate.slice(index, birth_month)
--- sub-array-division/select_chocolate_slices_matching_criteria.spec.rb
+++ sub-array-division/select_chocolate_slices_matching_criteria.spec.rb
@@ -1,23 +1,19 @@
-require 'test/unit'
+require_relative '../_utils/algorithm-test/index.rb'
 require_relative './select_chocolate_slices_matching_criteria.rb'
-require_relative '../_utils/array/array.rb'
 
 module HackerRank
   module Algorithms
-    class SelectChocolateSlicesMatchingCriteriaTest < Test::Unit::TestCase
-      include ArrayUtils
-      include HackerRank::Exceptions
+    class SelectChocolateSlicesMatchingCriteriaTest < AlgorithmTest
 
       def test_returns_zero_if_chocolate_array_is_empty
         result = Algorithms.select_chocolate_slices_matching_criteria(
           birth_day: 1, birth_month: 1, chocolate: []
         )
-
         assert_equal 0, result
       end
 
       def test_raises_an_exception_if_birth_day_is_smaller_than_one
-        assert_raises InvalidDayOfMonthException do
+        assert_raises_out_of_constraints do
           Algorithms.select_chocolate_slices_matching_criteria(
             birth_day: 0, birth_month: 1, chocolate: [1, 2]
           )
@@ -25,7 +21,7 @@ module HackerRank
       end
 
       def test_raises_an_exception_if_birth_day_is_greater_than_thirty_one
-        assert_raises InvalidDayOfMonthException do
+        assert_raises_out_of_constraints do
           Algorithms.select_chocolate_slices_matching_criteria(
             birth_day: 32, birth_month: 1, chocolate: [1, 2]
           )
@@ -33,7 +29,7 @@ module HackerRank
       end
 
       def test_raises_an_exception_if_birth_month_is_smaller_than_one
-        assert_raises InvalidMonthOfYearException do
+        assert_raises_out_of_constraints do
           Algorithms.select_chocolate_slices_matching_criteria(
             birth_day: 15, birth_month: 0, chocolate: [1, 2]
           )
@@ -41,16 +37,16 @@ module HackerRank
       end
 
       def test_raises_an_exception_if_birth_month_is_greater_than_twelve
-        assert_raises InvalidMonthOfYearException do
+        assert_raises_out_of_constraints do
           Algorithms.select_chocolate_slices_matching_criteria(
             birth_day: 15, birth_month: 13, chocolate: [1, 2]
           )
         end
       end
 
-      def test_raises_an_exception_if_array_size_is_greater_than_100_and_the_constrain_flag_is_disabled
-        ENV["ALLOW_UNCONSTRAINED_ARRAYS_ENABLED"] = "disabled"
-        assert_raises PossiblyUnboundArrayException do
+      def test_raises_an_exception_if_array_size_is_greater_than_100_and_ctt_v8n_enabled
+        ENV["CONSTRAINT_ENFORCEMENT_ENABLED"] = "enabled"
+        assert_raises ArrayOutOfConstraintsException do
           Algorithms.select_chocolate_slices_matching_criteria(
             birth_day: 15,
             birth_month: 10,
@@ -59,8 +55,8 @@ module HackerRank
         end
       end
 
-      def test_executes_if_array_length_is_greater_than_100_and_the_constrain_flag_is_enabled
-        ENV["ALLOW_UNCONSTRAINED_ARRAYS_ENABLED"] = "enabled"
+      def test_executes_if_array_length_is_greater_than_100_and_ctt_v8n_disabled
+        ENV["CONSTRAINT_ENFORCEMENT_ENABLED"] = "disabled"
         Algorithms.select_chocolate_slices_matching_criteria(
           birth_day: 15,
           birth_month: 10,
