06c7fc2463d3ee226175e6788efadccab9e50c00
Kaio Silveira <silveira.kaio@icloud.com>
2022-10-15 16:46:55 +0100

    angry-professor: update test class name to avoid clashes

--- angry-professor/algorithm.spec.rb
+++ angry-professor/algorithm.spec.rb
@@ -4,9 +4,7 @@ require_relative "../_utils/exceptions/exceptions.rb"
 
 module HackerRank
   module Algorithms
-    class AlgorithmTests < Test::Unit::TestCase
-      include HackerRank::Exceptions
-
+    class WasClassCanceledTests < Test::Unit::TestCase
       def test_applies_the_pattern_correctly
         assert_false Algorithms.was_class_canceled?(arrival_times: [-2, -1, 0, 1, 2], threshold: 3)
         assert Algorithms.was_class_canceled?(arrival_times: [-1, -3, 4, 2], threshold: 3)
