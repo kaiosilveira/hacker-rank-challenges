af99a87e74f3be44cb2fb447bd195b6a36cf7f26
Kaio Silveira <silveira.kaio@icloud.com>
2022-10-16 14:08:15 +0100

    beautiful-days: test refactoring

--- beautiful-days/count_beautiful_days.rb
+++ beautiful-days/count_beautiful_days.rb
@@ -1,21 +1,31 @@
 require_relative "../_utils/exceptions/exceptions.rb"
+require_relative "../_utils/validation/index.rb"
 
 module HackerRank
   module Algorithms
     include HackerRank::Exceptions
+    include HackerRank::Validation
 
     def self.count_beautiful_days(day0:, dayN:, divisor:)
       constraint_validation_enabled = ENV["CONSTRAINT_ENFORCEMENT_ENABLED"] == "enabled"
 
-      raise Exceptions::InputOutOfConstraintsException.new(
-        input_name: "divisor", constraints: "1 <= divisor <= 10^9",
-      ) if constraint_validation_enabled && is_divisor_out_of_constraints?(divisor)
+      validate_divisor(divisor)
 
-      raise Exceptions::InputOutOfConstraintsException.new(
-        input_name: "day0 | dayN", constraints: "1 <= day0 <= dayN <= 10^6",
-      ) if constraint_validation_enabled && (
-        day0 > dayN || is_day_range_out_of_constraints?(day0, dayN)
+      Validation.ensure_constraint_is_within_range(
+        constraint_name: "day0",
+        constraint_value: day0,
+        range: [1, 1_000_000],
       )
+
+      Validation.ensure_constraint_is_within_range(
+        constraint_name: "dayN",
+        constraint_value: dayN,
+        range: [1, 1_000_000],
+      )
+
+      raise Exceptions::InvalidInputFormatException.new(
+        input_name: "day0 | dayN", expected_format: "day0 <= dayN"
+      ) if day0 > dayN
 
       (day0..dayN).count do |n|
         reversed = n.to_s.reverse.to_i
@@ -25,12 +35,12 @@ module HackerRank
 
     private
 
-    def self.is_divisor_out_of_constraints?(divisor)
-      divisor < 1 || divisor > 2_000_000_000
-    end
-
-    def self.is_day_range_out_of_constraints?(day0, dayN)
-      [day0, dayN].any? { |n| n < 1 || n > 2_000_000 }
+    def self.validate_divisor(divisor)
+      Validation.ensure_constraint_is_within_range(
+        constraint_name: "divisor",
+        constraint_value: divisor,
+        range: [1, 1_000_000_000],
+      )
     end
   end
 end
--- beautiful-days/count_beautiful_days.spec.rb
+++ beautiful-days/count_beautiful_days.spec.rb
@@ -1,80 +1,58 @@
 require "test/unit"
 require_relative "./count_beautiful_days.rb"
-require_relative "../_utils/exceptions/exceptions.rb"
+require_relative "../_utils/algorithm-test/index.rb"
 
 module HackerRank
   module Algorithms
-    class CountBeautifulDaysTests < Test::Unit::TestCase
-      include HackerRank::Exceptions
-
-      def setup
-        ENV["CONSTRAINT_ENFORCEMENT_ENABLED"] = "enabled"
-      end
-
-      def teardown
-        ENV["CONSTRAINT_ENFORCEMENT_ENABLED"] = "enabled"
+    class CountBeautifulDaysTests < AlgorithmTest
+      def run_with(day0:, dayN:, divisor:)
+        Algorithms.count_beautiful_days(day0: day0, dayN: dayN, divisor: divisor)
       end
 
       def test_raises_an_exception_if_day0_is_lower_than_1
-        assert_raises Exceptions::InputOutOfConstraintsException do
-          Algorithms.count_beautiful_days(day0: 0, dayN: 1, divisor: 2)
-        end
+        assert_raises_out_of_constraints &-> { run_with(day0: 0, dayN: 1, divisor: 2) }
       end
 
       def test_raises_an_exception_if_dayN_is_lower_than_1
-        assert_raises Exceptions::InputOutOfConstraintsException do
-          Algorithms.count_beautiful_days(day0: 1, dayN: 0, divisor: 2)
-        end
+        assert_raises_out_of_constraints &-> { run_with(day0: 1, dayN: 0, divisor: 2) }
       end
 
       def test_raises_an_exception_if_day0_is_greater_than_dayN
-        assert_raises Exceptions::InputOutOfConstraintsException do
-          Algorithms.count_beautiful_days(day0: 2, dayN: 1, divisor: 2)
-        end
+        assert_raises_invalid_format &-> { run_with(day0: 2, dayN: 1, divisor: 2) }
       end
 
       def test_raises_an_exception_if_day0_is_greater_than_2_000_000
-        assert_raises Exceptions::InputOutOfConstraintsException do
-          Algorithms.count_beautiful_days(day0: 2_000_002, dayN: 2_000_000, divisor: 2)
-        end
+        assert_raises_out_of_constraints &-> { run_with(day0: 2_000_002, dayN: 2_000_003, divisor: 2) }
       end
 
       def test_raises_an_err_if_dayN_is_greater_than_2_000_000_and_ctt_v8n_enabled
-        assert_raises Exceptions::InputOutOfConstraintsException do
-          Algorithms.count_beautiful_days(day0: 2_000_000, dayN: 2_000_002, divisor: 2)
-        end
+        assert_raises_out_of_constraints &-> { run_with(day0: 2_000_000, dayN: 2_000_002, divisor: 2) }
       end
 
       def test_does_not_raise_ex_if_dayN_is_greater_than_2_000_000_and_ctt_v8n_disabled
         ENV["CONSTRAINT_ENFORCEMENT_ENABLED"] = "disabled"
-        Algorithms.count_beautiful_days(day0: 20, dayN: 2_000_002, divisor: 2)
+        run_with(day0: 20, dayN: 2_000_002, divisor: 2)
       end
 
       def test_raises_ex_if_divisor_is_lower_than_1
-        assert_raises Exceptions::InputOutOfConstraintsException do
-          Algorithms.count_beautiful_days(day0: 20, dayN: 23, divisor: 0)
-        end
+        assert_raises_out_of_constraints &-> { run_with(day0: 20, dayN: 23, divisor: 0) }
       end
 
       def test_raises_ex_if_divisor_is_greater_than_2_000_000_000
-        assert_raises Exceptions::InputOutOfConstraintsException do
-          Algorithms.count_beautiful_days(day0: 20, dayN: 23, divisor: 2_000_000_001)
-        end
+        assert_raises_out_of_constraints &-> { run_with(day0: 20, dayN: 23, divisor: 2_000_000_001) }
       end
 
       def test_raises_ex_if_divisor_is_greater_than_2_000_000_000_and_ctt_v8n_enabled
-        assert_raises Exceptions::InputOutOfConstraintsException do
-          Algorithms.count_beautiful_days(day0: 20, dayN: 23, divisor: 2_000_000_001)
-        end
+        assert_raises_out_of_constraints &-> { run_with(day0: 20, dayN: 23, divisor: 2_000_000_001) }
       end
 
       def test_does_not_raise_ex_if_divisor_is_greater_than_2_000_000_000_and_ctt_v8n_disabled
         ENV["CONSTRAINT_ENFORCEMENT_ENABLED"] = "disabled"
-        Algorithms.count_beautiful_days(day0: 20, dayN: 23, divisor: 2_000_000_001)
+        run_with(day0: 20, dayN: 23, divisor: 2_000_000_001)
       end
 
       def test_identifies_beautiful_days_correctly
-        assert_equal 2, Algorithms.count_beautiful_days(day0: 20, dayN: 23, divisor: 6)
+        assert_equal 2, run_with(day0: 20, dayN: 23, divisor: 6)
       end
     end
   end
