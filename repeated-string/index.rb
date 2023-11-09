5b34ab7d0c5f6f17b62b03c2b599bfb2da4ffd51
Kaio Silveira <silveira.kaio@icloud.com>
2023-11-09 12:53:09 +0000

    repeated-string: add sample

--- repeated-string/index.rb
+++ repeated-string/index.rb
@@ -4,6 +4,7 @@ require_relative "../_utils/hacker-rank/hacker-rank.rb"
 samples = [
   HackerRank.create_sample(expected_result: 4, params: ["abcabc", 10]),
   HackerRank.create_sample(expected_result: 4, params: ["abcab", 10]),
+  HackerRank.create_sample(expected_result: 7, params: ["aba", 10]),
 ]
 
 HackerRank::Runner.new(samples).run do |pattern, sequence_length|
